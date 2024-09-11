5a4cfa49802efe10e137802a2c90aefbe93a78b8
dimje <mark759@naver.com>
2024-09-11 16:09:21 +0900

    test : 네트워크 통신 테스트 코드 작성

--- data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.kt
+++ data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.kt
@@ -1,12 +1,22 @@
 package com.example.data
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.data.api.ForeignApi
+import com.example.data.api.ForeignXmlApi
+import com.example.data.datasource.ForeignRemoteDataSource
+import com.example.data.repository.foreign.ForeignInfoRepositoryImpl
+import com.google.gson.GsonBuilder
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +25,37 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+    val gson = GsonBuilder().setLenient().create()
+    val service = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(client)
+        .baseUrl("http://apis.data.go.kr")
+        .build()
+        .create(ForeignApi::class.java)
+    val xmlService = Retrofit.Builder()
+        .addConverterFactory(TikXmlConverterFactory.create())
+        .client(client)
+        .baseUrl("http://apis.data.go.kr")
+        .build()
+        .create(ForeignXmlApi::class.java)
+    val foreignInfoRepositoryImpl = ForeignInfoRepositoryImpl(ForeignRemoteDataSource(service, xmlService))
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.data.test", appContext.packageName)
+    fun 기본정보() = runBlocking {
+        foreignInfoRepositoryImpl.getBasicInfo(BuildConfig.DATA_POTAL_API_KEY, "일본")
+            .collect{ result ->
+                result.fold(
+                    onSuccess = { assert(it.countryName == "일본")
+                        println(it.basic)
+                    },
+                    onFailure = { println(it.message) }
+                )
+            }
+
     }
 }
--- data/src/test/java/com/example/data/ExampleUnitTest.kt
+++ data/src/test/java/com/example/data/ExampleUnitTest.kt
@@ -1,8 +1,21 @@
 package com.example.data
 
+import com.example.data.api.ForeignApi
+import com.example.data.api.ForeignXmlApi
+import com.example.data.datasource.ForeignRemoteDataSource
+import com.example.data.repository.foreign.ForeignInfoRepositoryImpl
+import com.google.gson.GsonBuilder
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +23,72 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+
+    val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+    val gson = GsonBuilder().setLenient().create()
+    val service = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(client)
+        .baseUrl("http://apis.data.go.kr")
+        .build()
+        .create(ForeignApi::class.java)
+    val xmlService = Retrofit.Builder()
+        .addConverterFactory(TikXmlConverterFactory.create())
+        .client(client)
+        .baseUrl("http://apis.data.go.kr")
+        .build()
+        .create(ForeignXmlApi::class.java)
+    val foreignInfoRepositoryImpl = ForeignInfoRepositoryImpl(ForeignRemoteDataSource(service, xmlService))
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun 입국조건() = runBlocking {
+
+        foreignInfoRepositoryImpl.getEntryCondition(BuildConfig.DATA_POTAL_API_KEY, "일본")
+            .collect{ result ->
+                result.fold(
+                    onSuccess = { assert(it.data.first().countryNm == "일본") },
+                    onFailure = { println(it.message) }
+                )
+            }
+
     }
+    @Test
+    fun 현지연락처() = runBlocking {
+
+        foreignInfoRepositoryImpl.getContact(BuildConfig.DATA_POTAL_API_KEY, "일본")
+            .collect{ result ->
+                result.fold(
+                    onSuccess = { assert(it.data.first().countryNm == "일본") },
+                    onFailure = { println(it.message) }
+                )
+            }
+
+    }
+    @Test
+    fun 기본정보() = runBlocking {
+        foreignInfoRepositoryImpl.getBasicInfo(BuildConfig.DATA_POTAL_API_KEY, "일본")
+            .collect{ result ->
+                result.fold(
+                    onSuccess = { assert(it.countryName == "일본")
+                                println(it.basic)
+                    },
+                    onFailure = { println(it.message) }
+                )
+            }
+
+    }
+    @Test
+    fun 치안상황() = runBlocking {
+        foreignInfoRepositoryImpl.getPolice(BuildConfig.DATA_POTAL_API_KEY, "일본")
+            .collect{ result ->
+                result.fold(
+                    onSuccess = { assert(it.data.first().countryNm == "일본") },
+                    onFailure = { println(it.message) }
+                )
+            }
+    }
+
 }
